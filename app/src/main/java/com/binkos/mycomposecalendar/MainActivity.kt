6be2eca5fe022f0ea681970af911e11fc957390d
binkos <vegas16777@gmail.com>
2021-10-05 17:19:05 +0300

    Initial commit

---
+++ app/src/main/java/com/binkos/mycomposecalendar/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.binkos.mycomposecalendar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.binkos.mycomposecalendar.ui.theme.MyComposeCalendarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeCalendarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyComposeCalendarTheme {
+        Greeting("Android")
+    }
+}
